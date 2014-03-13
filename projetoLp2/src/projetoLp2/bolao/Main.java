c9215daf6532193a1d98a0fec1f611afb71c02bb
oriondwl <oriondwl@lcc2-40.lcc.ufcg.edu.br>
2014-03-13 10:37:47 -0300

    //remoção comentário

--- projetoLp2/src/projetoLp2/bolao/Main.java
+++ projetoLp2/src/projetoLp2/bolao/Main.java
@@ -1,8 +1,7 @@
 package projetoLp2.bolao;
 
 public class Main {
-	
-	//helloooooooooooooooooo
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Oi grupo lasdfasdindo!");
