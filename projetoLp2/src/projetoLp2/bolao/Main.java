d2d302519c292df7209c79e6d1d2b69953e34f17
leticiamaia <leticiamaiat@gmail.com>
2014-03-13 10:53:15 -0300

    oooo

--- projetoLp2/src/projetoLp2/bolao/Main.java
+++ projetoLp2/src/projetoLp2/bolao/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Issoo eh um trabalho serio");
+		System.out.println("oooooooooooo");
 	}
 
 }
