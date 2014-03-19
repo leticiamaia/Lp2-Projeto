25ce07fdab1d069c4e0848739361657dd87defcc
Leticia <Leticia@192.168.1.3>
2014-03-19 18:00:26 -0300

    add Main

---
+++ projetoLp2/tests/projetoLp2/tests/TestaAdministrador.java
@@ -0,0 +1,11 @@
+package projetoLp2.tests;
+
+import projetoLp2.bolao.Administrador;
+
+public class TestaAdministrador {
+	Administrador admin = new Administrador ("admin", "senha");
+	public void testaCadastraPartida() {
+	
+	}
+	
+}
