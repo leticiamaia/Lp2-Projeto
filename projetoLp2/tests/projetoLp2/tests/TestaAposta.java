b732991413e2af0912ae996c2a0ef2072cbf6dde
Leticia <Leticia@192.168.1.3>
2014-04-02 22:02:09 -0300

    Aposta so pode ser feita na Data certa

--- projetoLp2/tests/projetoLp2/tests/TestaAposta.java
+++ projetoLp2/tests/projetoLp2/tests/TestaAposta.java
@@ -1,5 +1,6 @@
 package projetoLp2.tests;
 
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 import org.junit.Assert;
@@ -17,32 +18,33 @@ import projetoLp2.bolao.TimeCopa;
 public class TestaAposta {
 
 	@Test
-	public void testaConstrutor() throws Exception{
+	public void testaConstrutor() throws Exception {
 		TimeCopa time1 = new TimeCopa("bandeiraHolanda.png", "Holanda", "HOL");
 		TimeCopa time2 = new TimeCopa("bandeiraBrasil.png", "Brasil", "BRA");
-		Partida partida = new Partida(time1, time2, new GregorianCalendar(2014, 8, 15, 15, 30));
-		
+		Partida partida = new Partida(time1, time2, new GregorianCalendar(2014,
+				8, 15, 15, 30));
+
 		try {
 			new ApostaPrimeiraFase(null, 1, 2);
 			Assert.fail();
 		} catch (Exception e) {
 			Assert.assertEquals("Partida invalida!", e.getMessage());
 		}
-	
+
 		try {
 			new ApostaPrimeiraFase(partida, -1, 2);
 			Assert.fail();
 		} catch (Exception e) {
 			Assert.assertEquals("Palpite de gols invalido!", e.getMessage());
 		}
-		
+
 		try {
 			new ApostaPrimeiraFase(partida, 1, -2);
 			Assert.fail();
 		} catch (Exception e) {
 			Assert.assertEquals("Palpite de gols invalido!", e.getMessage());
 		}
-		
+
 		try {
 			Aposta aposta = new ApostaPrimeiraFase(partida, 1, 2);
 			Assert.assertTrue(aposta.getPalpiteGolsTime1() == 1);
@@ -50,21 +52,34 @@ public class TestaAposta {
 		} catch (Exception e) {
 			Assert.fail();
 		}
+
+		try {
+			GregorianCalendar c = new GregorianCalendar();
+			c.add(Calendar.MINUTE, 60);
+			Partida partida1 = new Partida(time1, time2, c);
+			new ApostaPrimeiraFase(partida1, 1, 2);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals(
+					"Nao e mais possivel fazer uma aposta nessa partida.",
+					e.getMessage());
+		}
 	}
-	
+
 	@Test
-	public void testaSetPalpite() throws Exception{
+	public void testaSetPalpite() throws Exception {
 		TimeCopa time1 = new TimeCopa("bandeiraHolanda.png", "Holanda", "HOL");
 		TimeCopa time2 = new TimeCopa("bandeiraBrasil.png", "Brasil", "BRA");
-		Partida partida = new Partida(time1, time2, new GregorianCalendar(2014, 8, 15, 15, 30));		
-		
+		Partida partida = new Partida(time1, time2, new GregorianCalendar(2014,
+				8, 15, 15, 30));
+
 		try {
 			Aposta aposta = new ApostaPrimeiraFase(partida, 1, 2);
 			aposta.setPalpiteGolsTime1(-1);
 		} catch (Exception e) {
 			Assert.assertEquals("Palpite de gols invalido!", e.getMessage());
 		}
-		
+
 		try {
 			Aposta aposta = new ApostaPrimeiraFase(partida, 1, 2);
 			aposta.setPalpiteGolsTime2(-2);
@@ -74,30 +89,31 @@ public class TestaAposta {
 
 		try {
 			Aposta aposta = new ApostaPrimeiraFase(partida, 1, 2);
-			
+
 			aposta.setPalpiteGolsTime1(0);
 			Assert.assertTrue(aposta.getPalpiteGolsTime1() == 0);
-			
+
 			aposta.setPalpiteGolsTime2(5);
 			Assert.assertTrue(aposta.getPalpiteGolsTime2() == 5);
 		} catch (Exception e) {
 			Assert.fail();
 		}
 	}
-	
+
 	@Test
-	public void testaResultadoAposta() throws Exception{
+	public void testaResultadoAposta() throws Exception {
 		TimeCopa time1 = new TimeCopa("bandeiraHolanda.png", "Holanda", "HOL");
 		TimeCopa time2 = new TimeCopa("bandeiraBrasil.png", "Brasil", "BRA");
-		Partida partida = new Partida(time1, time2, new GregorianCalendar(2014, 8, 15, 15, 30));
-		
+		Partida partida = new Partida(time1, time2, new GregorianCalendar(2014,
+				8, 15, 15, 30));
+
 		try {
 			Aposta aposta = new ApostaOitavasDeFinal(partida, 1, 1);
 			aposta.resultadoAposta();
 		} catch (Exception e) {
 			Assert.assertEquals("Jogo ainda nao foi realizado!", e.getMessage());
 		}
-		
+
 		try {
 			partida.setGols(3, 3);
 			Aposta aposta = new ApostaPrimeiraFase(partida, 5, 5);
@@ -105,7 +121,7 @@ public class TestaAposta {
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		
+
 		try {
 			partida.setGols(3, 3);
 			Aposta aposta = new ApostaPrimeiraFase(partida, 3, 3);
@@ -113,7 +129,7 @@ public class TestaAposta {
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		
+
 		try {
 			partida.setGols(3, 3);
 			Aposta aposta = new ApostaPrimeiraFase(partida, 4, 5);
@@ -121,7 +137,7 @@ public class TestaAposta {
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		
+
 		try {
 			partida.setGols(3, 3);
 			Aposta aposta = new ApostaPrimeiraFase(partida, 3, 5);
@@ -129,7 +145,7 @@ public class TestaAposta {
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		
+
 		try {
 			partida.setGols(3, 3);
 			Aposta aposta = new ApostaPrimeiraFase(partida, 5, 3);
@@ -137,7 +153,7 @@ public class TestaAposta {
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		
+
 		try {
 			partida.setGols(4, 2);
 			Aposta aposta = new ApostaOitavasDeFinal(partida, 4, 2);
@@ -145,7 +161,7 @@ public class TestaAposta {
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		
+
 		try {
 			partida.setGols(4, 2);
 			Aposta aposta = new ApostaOitavasDeFinal(partida, 3, 2);
@@ -153,7 +169,7 @@ public class TestaAposta {
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		
+
 		try {
 			partida.setGols(4, 2);
 			Aposta aposta = new ApostaOitavasDeFinal(partida, 3, 1);
@@ -161,7 +177,7 @@ public class TestaAposta {
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		
+
 		try {
 			partida.setGols(4, 2);
 			Aposta aposta = new ApostaOitavasDeFinal(partida, 1, 1);
@@ -169,7 +185,7 @@ public class TestaAposta {
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		
+
 		try {
 			partida.setGols(3, 4);
 			Aposta aposta = new ApostaQuartasDeFinal(partida, 3, 4);
@@ -177,7 +193,7 @@ public class TestaAposta {
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		
+
 		try {
 			partida.setGols(3, 4);
 			Aposta aposta = new ApostaQuartasDeFinal(partida, 2, 4);
@@ -185,7 +201,7 @@ public class TestaAposta {
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		
+
 		try {
 			partida.setGols(3, 4);
 			Aposta aposta = new ApostaQuartasDeFinal(partida, 2, 3);
@@ -193,7 +209,7 @@ public class TestaAposta {
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		
+
 		try {
 			partida.setGols(3, 4);
 			Aposta aposta = new ApostaQuartasDeFinal(partida, 2, 2);
@@ -201,7 +217,7 @@ public class TestaAposta {
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		
+
 		try {
 			partida.setGols(3, 4);
 			Aposta aposta = new ApostaSemiFinal(partida, 3, 4);
@@ -209,7 +225,7 @@ public class TestaAposta {
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		
+
 		try {
 			partida.setGols(3, 4);
 			Aposta aposta = new ApostaFinal(partida, 3, 4);
@@ -218,5 +234,5 @@ public class TestaAposta {
 			Assert.fail();
 		}
 	}
-	
+
 }
