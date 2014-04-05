f7a677b414f52de167b6b302bfe8f6cea1350438
ldmatos <ldmatos8@gmail.com>
2014-04-05 12:19:09 -0300

    Tela de cadastrar jogo incompleta.

--- projetoLp2/src/interfaceGrafica/TelaDeCadastrarJogo.java
+++ projetoLp2/src/interfaceGrafica/TelaDeCadastrarJogo.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,6 +15,7 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
@@ -25,8 +28,9 @@ public class TelaDeCadastrarJogo extends JFrame {
 	private static final long serialVersionUID = -239342797923874L;
 	private JPanel contentPane;
 	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JTextField abrevTime1;
+	private JTextField abrevTime2;
+	private String time1, time2, data;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -58,15 +62,15 @@ public class TelaDeCadastrarJogo extends JFrame {
 		TelaDoAdministradorLabel.setFont(new Font("Segoe Print", Font.PLAIN, 32));
 		contentPane.add(TelaDoAdministradorLabel);
 		
-		textField = new JTextField();
-		textField.setBounds(319, 302, 137, 36);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		abrevTime1 = new JTextField();
+		abrevTime1.setBounds(319, 302, 137, 36);
+		contentPane.add(abrevTime1);
+		abrevTime1.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(839, 302, 137, 36);
-		contentPane.add(textField_1);
+		abrevTime2 = new JTextField();
+		abrevTime2.setColumns(10);
+		abrevTime2.setBounds(839, 302, 137, 36);
+		contentPane.add(abrevTime2);
 		
 		JLabel lblNewLabel = new JLabel("Time1");
 		lblNewLabel.setBounds(286+33, 250, 223, 40);
@@ -76,14 +80,29 @@ public class TelaDeCadastrarJogo extends JFrame {
 		label.setBounds(791+45, 250, 223, 40);
 		contentPane.add(label);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(577, 302, 137, 36);
-		contentPane.add(textField_2);
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(577, 302, 137, 36);
+		contentPane.add(textField);
 		
 		JLabel lblNewLabel_1 = new JLabel("DD/MM/AAAA");
 		lblNewLabel_1.setBounds(577, 263, 187, 15);
 		contentPane.add(lblNewLabel_1);
+		
+		JButton btnCadastrar = new JButton("CADASTRAR!");
+		btnCadastrar.setBounds(522, 411, 246, 52);
+		contentPane.add(btnCadastrar);
+		btnCadastrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				time1 = abrevTime1.getText();
+				time2 = abrevTime2.getText();
+				data = textField.getText();
+				JOptionPane.showMessageDialog(null,
+						"Nada foi feito com sucesso!");
+			}
+		});
+		
+		
 	}
 }
 
