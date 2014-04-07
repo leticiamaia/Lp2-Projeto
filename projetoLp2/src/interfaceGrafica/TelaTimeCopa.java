b0b426c290869add21356053d661a92f4f13f55a
odwl66 <orion.lima@ccc.ufcg.edu.br>
2014-04-07 16:38:27 -0300

    Ajeitando progressBar

--- projetoLp2/src/interfaceGrafica/TelaTimeCopa.java
+++ projetoLp2/src/interfaceGrafica/TelaTimeCopa.java
@@ -9,7 +9,6 @@ import java.util.List;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -21,9 +20,8 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.SwingConstants;
-import javax.swing.JList;
-import javax.swing.JScrollPane;
 import javax.swing.JButton;
+import javax.swing.JProgressBar;
 
 public class TelaTimeCopa extends JPanel {
 
@@ -32,15 +30,12 @@ public class TelaTimeCopa extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 	private final JPanel mainPanel = new JPanel(new CardLayout());
-	//private static TimeCopa[] times = (TimeCopa[]) ControladorTimes.ler().values().toArray();
-	//private static Map<String, TimeCopa> mapTimes = (Map<String, TimeCopa>) ControladorTimes.ler();
-	//private static Collection<TimeCopa> listaDeTimes = (Collection<TimeCopa>) mapTimes.values();
 	private Collection<TimeCopa> listaDeTimes = (Collection<TimeCopa>) ControladorTimes.ler().values();
 	private final String[] comboItens = arrayComboItens();
-	private JComboBox comboBox = new JComboBox(comboItens);
+	private JComboBox<String> comboBox = new JComboBox<String>(comboItens);
 	private TimeCopa timeSelecionadoAtual;
 	private JPanel panelDasPartidas = new JPanel();
-	//private JList listaDasPartidasJogadas = new JList();
+	private final JProgressBar barraEmpate = new JProgressBar(), barraVitoria = new JProgressBar(), barraDerrota = new JProgressBar();
 	/**
 	 * Create the panel.
 	 */
@@ -97,7 +92,7 @@ public class TelaTimeCopa extends JPanel {
 		
 		final JLabel labelNumeroDeVitorias = new JLabel("");
 		labelNumeroDeVitorias.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		labelNumeroDeVitorias.setBounds(130, 92, 110, 33);
+		labelNumeroDeVitorias.setBounds(130, 92, 58, 33);
 		panelTeste.add(labelNumeroDeVitorias);
 		
 		JLabel lblNmeroDeDerrotas = new JLabel("N\u00BA de Derrotas:");
@@ -107,17 +102,17 @@ public class TelaTimeCopa extends JPanel {
 		
 		final JLabel labelNumeroDeDerrotas = new JLabel("");
 		labelNumeroDeDerrotas.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		labelNumeroDeDerrotas.setBounds(130, 136, 115, 27);
+		labelNumeroDeDerrotas.setBounds(130, 136, 58, 27);
 		panelTeste.add(labelNumeroDeDerrotas);
 		
 		JLabel lblNDeEmpates = new JLabel("N\u00BA de Empates:");
 		lblNDeEmpates.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNDeEmpates.setBounds(10, 181, 155, 30);
+		lblNDeEmpates.setBounds(10, 174, 155, 30);
 		panelTeste.add(lblNDeEmpates);
 		
 		final JLabel labelNumeroDeEmpates = new JLabel("");
 		labelNumeroDeEmpates.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		labelNumeroDeEmpates.setBounds(130, 184, 115, 27);
+		labelNumeroDeEmpates.setBounds(130, 184, 58, 27);
 		panelTeste.add(labelNumeroDeEmpates);
 		
 		JLabel lblPartidasJogadas = new JLabel("Partidas Jogadas:");
@@ -131,6 +126,18 @@ public class TelaTimeCopa extends JPanel {
 		panelDasPartidas.setVisible(true);
 		panelTeste.add(panelDasPartidas);
 		panelDasPartidas.setLayout(null);
+		
+		barraDerrota.setStringPainted(true);
+		barraDerrota.setBounds(170, 138, 131, 27);
+		panelTeste.add(barraDerrota);
+		
+		barraVitoria.setStringPainted(true);
+		barraVitoria.setBounds(170, 97, 131, 27);
+		panelTeste.add(barraVitoria);
+		
+		barraEmpate.setStringPainted(true);
+		barraEmpate.setBounds(170, 179, 131, 27);
+		panelTeste.add(barraEmpate);
 		
 		JLabel selecioneFase = new JLabel("Selecione o time que deseja visualizar:");
 		selecioneFase.setFont(new Font("Tahoma", Font.PLAIN, 14));
@@ -152,6 +159,7 @@ public class TelaTimeCopa extends JPanel {
 				labelNumeroDeDerrotas.setText(numeroDeDerrotas());
 				labelNumeroDeEmpates.setText(numeroDeEmpates());
 				addAPartidasJogadas();
+				barrasDePorcentagem();
 			}
 		});
 		btnOK.setBounds(932, 111, 64, 26);
@@ -168,8 +176,41 @@ public class TelaTimeCopa extends JPanel {
 		}
 		
 		return null;
-		
 	}
+	
+	private void barrasDePorcentagem(){
+		 aumentaBarra(timeSelecionadoAtual.getPorcentagemVitorias(), barraVitoria);
+		 aumentaBarra(timeSelecionadoAtual.getPorcentagemDerrotas(), barraDerrota);
+		 aumentaBarra(timeSelecionadoAtual.getPorcentagemEmpates(), barraEmpate);
+	}
+	
+	private void aumentaBarra(final int porcentagem, final JProgressBar barra){
+			if (barra.equals(barraVitoria)){
+				if (porcentagem < 50)
+					barra.setForeground(Color.RED);
+				else
+					barra.setForeground(Color.GREEN);
+			}
+			else if (barra.equals(barraDerrota)) {
+				if (porcentagem < 50)
+					barra.setForeground(Color.GREEN);
+				else
+					barra.setForeground(Color.RED);
+			}
+			
+			new Thread(){
+				public void run(){
+					for (int i = 0; i <= porcentagem; i++) {
+						try {
+							sleep(25);
+							barra.setValue(i);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}.start();
+		}
 	
 	private String nomeDoTime(){
 		if (timeSelecionadoAtual == null)
@@ -186,26 +227,25 @@ public class TelaTimeCopa extends JPanel {
 	private String numeroDeVitorias() {
 		if (timeSelecionadoAtual == null)
 			return "";
-		return timeSelecionadoAtual.getNumVitorias() + " ("+(timeSelecionadoAtual.getPorcentagemVitorias())+"%)";
+		return timeSelecionadoAtual.getNumVitorias()+"";
 	}
 	
 	private String numeroDeDerrotas() {
 		if (timeSelecionadoAtual == null)
 			return "";
-		return timeSelecionadoAtual.getNumDerrotas() + " ("+(timeSelecionadoAtual.getPorcentagemDerrotas())+"%)";
+		return timeSelecionadoAtual.getNumDerrotas() + "";
 	}
 	
 	private String numeroDeEmpates() {
 		if (timeSelecionadoAtual == null)
 			return "";
-		return timeSelecionadoAtual.getNumEmpates() + " ("+(timeSelecionadoAtual.getPorcentagemEmpates())+"%)";
+		return timeSelecionadoAtual.getNumEmpates() + "";
 	}
 	
 	private String[] arrayComboItens(){
 		String[] arrayComboItens = new String[32];
 		
 		int i = 0;
-		//ordenaTimes();
 		for (TimeCopa time : listaDeTimes) {
 			arrayComboItens[i] = time.toString();
 			i++;
@@ -213,25 +253,11 @@ public class TelaTimeCopa extends JPanel {
 		return arrayComboItens;
 	}
 	
-	/*private void adicionaItensAoComboBox(){
-		JPanel panelDoTime;
-		
-		for (int i = 1; i < comboItens.length; i++) {
-			panelDoTime = new JPanel(null);
-			panelDoTime.setBounds(0, 0, 822, 452);
-			mainPanel.add(panelDoTime, comboItens[i]);
-		}
-	}*/
-	
 	private Icon iconeDoTime() {
 		if (timeSelecionadoAtual == null)
 			return null;
 		return timeSelecionadoAtual.getBandeiraDoTime();
 	}
-	
-	/*private static void ordenaTimes(){
-		Arrays.sort(arrayTimes);
-	}*/
 	
 	private void addAPartidasJogadas(){
 		List<Partida> partidasJogadas = timeSelecionadoAtual.getPartidasJogadas();
@@ -249,20 +275,5 @@ public class TelaTimeCopa extends JPanel {
 				panelDasPartidas.add(partida.panelDaPartida());
 			}
 		}
-	}
-	
-	public static void main(String[] args) {
-		/*for (int i = 0; i < comboItens.length; i++) {
-			System.out.println(comboItens[i]);
-		}
-		
-		for (TimeCopa time : listaDeTimes) {
-			System.out.println(time);
-		}*/ 
-		JFrame janela = new JFrame();
-		janela.setBounds(0, 0, 1300, 700);
-		janela.setVisible(true);
-		TelaTimeCopa panel = new TelaTimeCopa();
-		janela.getContentPane().add(panel);
 	}
 }
