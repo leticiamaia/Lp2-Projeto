a1f5aac48d61c2a3e1adc2ed630fc4e3748b58f2
Leticia <Leticia@192.168.1.3>
2014-04-08 05:49:36 -0300

    aa

--- projetoLp2/src/interfaceGrafica/TelaDoUsuario.java
+++ projetoLp2/src/interfaceGrafica/TelaDoUsuario.java
@@ -1,14 +1,12 @@
 package interfaceGrafica;
 
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import javax.swing.Box;
 import javax.swing.ImageIcon;
